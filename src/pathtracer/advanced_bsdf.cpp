b0ff8153cc8d5ce92a9e480bf8b41f6a1b085319
hyang23333 <yanghuang@Yangs-MacBook-Pro.local>
2022-03-24 16:26:05 -0700

    part1

--- src/pathtracer/advanced_bsdf.cpp
+++ src/pathtracer/advanced_bsdf.cpp
@@ -3,12 +3,15 @@
 #include <algorithm>
 #include <iostream>
 #include <utility>
+#include <math.h>
 
 #include "application/visual_debugger.h"
 
 using std::max;
 using std::min;
 using std::swap;
+using std::abs;
+//using namespace std;
 
 namespace CGL {
 
@@ -22,7 +25,9 @@ Vector3D MirrorBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
 
   // TODO Project 3-2: Part 1
   // Implement MirrorBSDF
-  return Vector3D();
+  *pdf = 1;
+  reflect(wo, wi);
+  return reflectance/std::fabs(wi->z);
 }
 
 void MirrorBSDF::render_debugger_node()
@@ -92,7 +97,16 @@ Vector3D RefractionBSDF::f(const Vector3D wo, const Vector3D wi) {
 Vector3D RefractionBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
   // TODO Project 3-2: Part 1
   // Implement RefractionBSDF
-  return Vector3D();
+  if(refract(wo, wi, ior)){
+    float eta = ior;;
+    if (wo[2] > 0) {
+        eta = 1/ior;
+    }
+    return transmittance / abs_cos_theta(*wi) / (eta*eta);
+
+  }else{
+    return Vector3D();
+  }
 }
 
 void RefractionBSDF::render_debugger_node()
@@ -118,7 +132,38 @@ Vector3D GlassBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
 
   // compute Fresnel coefficient and use it as the probability of reflection
   // - Fundamentals of Computer Graphics page 305
-  return Vector3D();
+    *pdf = 1;
+
+    // Get the initial refract direction.
+    bool res = refract(wo, wi, ior);
+    if (!res) {
+        return transmittance * (1/std::max(std::fabs((*wi)[2]),1e-8));
+    }
+    
+    
+    // Calculating Fr
+    double ni = ior;
+    double no = 1;
+    double cos_i = std::fabs((*wi)[2]);
+    double cos_o = std::fabs(wo[2]);
+    if (wo[2] < 0) {
+        swap(ni,no);
+    }
+    
+    double r1 = (no*cos_i - ni*cos_o)/(no*cos_i + ni*cos_o);
+    double r2 = (ni*cos_i - no*cos_o)/(ni*cos_i + no*cos_o);
+    double Fr = 0.5*(r1*r1 + r2*r2);
+
+    if (std::rand() / (double)RAND_MAX <= Fr) {    // If we choose reflection
+        reflect(wo, wi);
+        // Here we don't need to multiply Fr because we already using randomized strategy to achieve it.
+        return reflectance * (1/std::max(std::fabs((*wi)[2]),1e-8));
+    }
+    else{                       // If we choose refraction
+        double ratio = no/ni;
+        // Here we don't need to multiply (1-Fr) because we already using randomized strategy to achieve it.
+        return transmittance * ratio*ratio * (1/std::max(std::fabs((*wi)[2]),1e-8));
+    }
 }
 
 void GlassBSDF::render_debugger_node()
@@ -136,7 +181,9 @@ void BSDF::reflect(const Vector3D wo, Vector3D* wi) {
 
   // TODO Project 3-2: Part 1
   // Implement reflection of wo about normal (0,0,1) and store result in wi.
-
+  Vector3D n = Vector3D(0.0, 0.0, 1.0);
+ // *wi = (2 * dot(wo, n) * n) - wo;
+  *wi = Vector3D(-wo[0],-wo[1],wo[2]);
 
 }
 
@@ -147,6 +194,26 @@ bool BSDF::refract(const Vector3D wo, Vector3D* wi, double ior) {
   // Return false if refraction does not occur due to total internal reflection
   // and true otherwise. When dot(wo,n) is positive, then wo corresponds to a
   // ray entering the surface through vacuum.
+      int sign = 1;
+    float eta;
+   // When z is positive,  we are entering the non-air material,
+   //
+    if (wo[2] > 0) {
+        sign = -1;
+        eta = 1/ior;
+    }
+    else{
+      eta = ior;
+    }
+    
+    float cos2_wi = 1 - eta*eta*(1 - wo[2]*wo[2]);
+    if (cos2_wi < 0) {
+        *wi = Vector3D(-wo[0],-wo[1],wo[2]);
+        return false;
+    }
+
+    *wi = Vector3D(-wo[0]*eta,-wo[1]*eta, sign * sqrt(cos2_wi)).unit();
+
 
   return true;
 
